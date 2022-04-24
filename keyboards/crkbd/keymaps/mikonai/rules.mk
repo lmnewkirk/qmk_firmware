c0f743087ae91d2c1655d2ddb3b69ef8ad38a2bd
lmnewkirk <lydia.m.newkirk@gmail.com>
2022-04-23 20:03:59 -0400

    adding my own keymaps for crkbd

---
+++ keyboards/crkbd/keymaps/mikonai/rules.mk
@@ -0,0 +1,8 @@
+BOOTLOADER = atmel-dfu
+MOUSEKEY_ENABLE = yes    # Mouse keys
+RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+OLED_ENABLE     = no
+OLED_DRIVER     = SSD1306
+LTO_ENABLE      = yes
+TAP_DANCE_ENABLE = yes
+
--- keyboards/kprepublic/bm40hsrgb/keymaps/mikonai/rules.mk
+++ keyboards/kprepublic/bm40hsrgb/keymaps/mikonai/rules.mk
@@ -1,9 +1,13 @@
 #rules
 
-#WPM_ENABLE = yes
-MOUSEKEY_ENABLE = yes # Mouse keys
-BOOTMAGIC_ENABLE = lite     # Enable Bootmagic Lite
+WPM_ENABLE = no
+MOUSEKEY_ENABLE = no # Mouse keys
+COMMAND_ENABLE = no # no commands? not sure what this does but might save me space
+BOOTMAGIC_ENABLE = yes     # Enable Bootmagic Lite
+CONSOLE_ENABLE = no
 EXTRAKEY_ENABLE = yes # Audio control and System control
 TAP_DANCE_ENABLE = yes
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
+EXTRAFLAGS += -flto
+MIDI_ENABLE = yes
